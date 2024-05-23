2f2d86a75c305f7ab128f83040c38f8ed55c6482
Baldomero Cho <don.baldo@hotmail.com>
2024-05-23 18:39:11 +0200

    correcciones y Dockerfile

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -43,16 +43,22 @@ export const initWbot = async (whatsapp: Whatsapp): Promise<Session> => {
         sessionCfg = JSON.parse(whatsapp.session);
       }
 
-      const args:String = process.env.CHROME_ARGS || "";
+      const args: string = process.env.CHROME_ARGS || "";
+
+      const wwebVersion = "2.2407.3";
 
       const wbot: Session = new Client({
         session: sessionCfg,
-        authStrategy: new LocalAuth({clientId: 'bd_'+whatsapp.id}),
-        puppeteer: {
+	authStrategy: new LocalAuth({ clientId: `bd_${whatsapp.id}` }),
+	puppeteer: {
           executablePath: process.env.CHROME_BIN || undefined,
           // @ts-ignore
           browserWSEndpoint: process.env.CHROME_WS || undefined,
           args: args.split(' ')
+        },
+	webVersionCache: {
+          type: "remote",
+          remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`
         }
       });
 
